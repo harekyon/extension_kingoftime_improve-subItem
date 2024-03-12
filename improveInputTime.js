5c502adc13070b9ac9b07427b2975fbcae01351c
harekyon <harekyon@gmail.com>
2024-03-12 13:45:21 +0900

    時間をワンクリックで入力できるように

--- improveInputTime.js
+++ improveInputTime.js
@@ -1,12 +1,8 @@
-// const getTimeTable = document.getElementsByClassName("specific-table_600");
-// const addSectionButton = document.getElementById("supplemental_working_record_add");
 let getTimeTable = document.getElementsByTagName("td");
-// console.log(getTimeTable);
 const pattern = /^\d{4}年 \d{2}月 \d{2}日 \d{2}時 \d{2}分$/g;
 
 let formatedTime = [];
 Array.from(getTimeTable).map((t) => {
-  // console.log(t.innerText);
   if (t.innerText.match(pattern)) {
     const date = t.innerText;
     const hourArrayNum = date.indexOf("時");
@@ -17,8 +13,9 @@ Array.from(getTimeTable).map((t) => {
     formatedTime.push(timeResult);
   }
 });
-let inputTimeElement = [];
+let inputTimeElement;
 function getInputElement() {
+  inputTimeElement = [];
   const inputElementArray = Array.from(document.getElementsByTagName("input"));
   inputElementArray.map((i) => {
     if (
@@ -30,28 +27,69 @@ function getInputElement() {
       inputTimeElement.push(i);
     }
   });
-  // console.log(inputTimeElement);
 }
 
-function addButtonTimeElement() {
-  inputTimeElement.map((i) => {
-    i.insertAdjacentElement("afterend", createTimeButton());
+function addTimeButtonElement() {
+  inputTimeElement.map((i, idx) => {
+    i.insertAdjacentElement("afterend", createTimeButton(idx));
   });
 }
-function createTimeButton() {
+function addTimeButtonEleWhenClicAddItem() {
+  inputTimeElement.map((i, idx) => {
+    //項目を追加した場合は、追加分だけボタンを増やしたいので、以下の条件式を追加
+    if (inputTimeElement.length - 2 <= idx) {
+      i.insertAdjacentElement("afterend", createTimeButton(idx));
+    }
+  });
+}
+function createTimeButton(num) {
   const buttonTimeButtonWrapper = document.createElement("div");
   formatedTime.map((f) => {
     const buttonTime = document.createElement("button");
     buttonTime.innerText = f;
+    buttonTime.type = "button";
     buttonTime.style.fontSize = "10px";
     buttonTime.style.width = "auto";
     buttonTime.style.padding = "0 2px";
+    buttonTime.tabIndex = "-1";
     buttonTimeButtonWrapper.appendChild(buttonTime);
+    buttonTime.onclick = function () {
+      inputTimeElement[num].value = f;
+      const startTimeText = "start_time_";
+      const endTimeText = "end_time_";
+
+      if (inputTimeElement[num].id.includes(startTimeText)) {
+        const eigenvalue = inputTimeElement[num].id.substring(startTimeText.length);
+        let selectDayElement = document.getElementById(`start_time_day_${eigenvalue}`);
+        judgeNextDay(selectDayElement, f);
+      }
+      if (inputTimeElement[num].id.includes(endTimeText)) {
+        const eigenvalue = inputTimeElement[num].id.substring(endTimeText.length);
+        let selectDayElement = document.getElementById(`end_time_day_${eigenvalue}`);
+        judgeNextDay(selectDayElement, f);
+      }
+    };
   });
   buttonTimeButtonWrapper.style.display = "grid";
   buttonTimeButtonWrapper.style.gridTemplateColumns = "repeat(4, 37.2px)";
   buttonTimeButtonWrapper.style.marginLeft = "73px";
   return buttonTimeButtonWrapper;
 }
+function judgeNextDay(selectDayElement, f) {
+  if (0 <= parseFloat(f) && parseFloat(f) < 5) {
+    Array.from(selectDayElement.children).map((s) => {
+      if (s.innerText === "翌日") {
+        s.selected = true;
+      }
+    });
+  } else if (5 <= parseFloat(f) && parseFloat(f) < 24) {
+    Array.from(selectDayElement.children).map((s) => {
+      if (s.innerText === "当日") {
+        s.selected = true;
+      }
+    });
+  }
+}
+
 getInputElement();
-addButtonTimeElement();
+addTimeButtonElement();
